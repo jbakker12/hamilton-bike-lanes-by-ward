a876fe6d69ef7ee63d718029ffae9fa1bf396bdf
Jake Bakker <jacob.bakker1@mohawkcollege.ca>
2025-09-11 14:00:00 -0400

    General  Page Stucture

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Bike Lanes by Ward in Hamilton</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+          This page shows bike lanes across the City of Hamilton's 15 wards in map and chart format. </p>
+          <p>The goal of this page is to highlight the distribution of bike lanes, particularly highlighting the number of bike lanes in wards with higher poverty rates.</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div class = "map">
+
+      </div>
+      <div class = "chart">
+        
+      </div>
     </>
   )
 }
